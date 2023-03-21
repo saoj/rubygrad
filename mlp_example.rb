b37b652ffabe098aa4b1984f7133c15b458c674f
Sergio Oliveira Jr <sergio.oliveira.jr@gmail.com>
2023-03-21 19:26:02 -0300

    Improvements

--- mlp_example.rb
+++ mlp_example.rb
@@ -11,30 +11,31 @@ x_inputs = [
 y_expected = [1.0, -1.0, -1.0, 1.0] # desired
 
 passes = 2000
-passes_format = "%#{passes.digits.length}d"
-loss_precision = 10
-loss_format = "%.#{loss_precision}f"
 learning_rate = 0.2
+
+_loss_precision = 10
+_passes_format = "%#{passes.digits.length}d"
+_loss_format = "%.#{_loss_precision}f"
 
 (0...passes).each do |pass| 
 
-    # forward pass
+    # forward pass (calculate output)
     y_calculated = x_inputs.map { |x| nn.calc(x, :tanh) }
 
-    # loss function
+    # loss function (check how good the neural net is)
     loss = 0.0
     y_expected.each_index { |i| loss += (y_calculated[i] - y_expected[i]) ** 2 }
 
-    # backward pass
+    # backward pass (calculate gradients)
     nn.zero_grad
     loss.backward
 
-    # improve
+    # improve neural net (update weights and biases)
     nn.parameters.each { |p| p.value -= learning_rate * p.grad }
 
-    puts "Pass #{passes_format % (pass + 1)} => Learning rate: #{"%.10f" % learning_rate} => Loss: #{loss_format % loss.value}" if (pass + 1) % 20 == 0 or pass == 0
+    puts "Pass #{_passes_format % (pass + 1)} => Learning rate: #{"%.10f" % learning_rate} => Loss: #{_loss_format % loss.value}" if (pass + 1) % 20 == 0 or pass == 0
 
-    break if loss.value == 0 # just for fun just in case
+    break if loss.value == 0 # just for fun and just in case
 end
 
 y_calculated = x_inputs.map { |x| nn.calc(x, :tanh) }
