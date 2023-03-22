ca5cc730b4568e236c22c7f74ec6d4314c62c5eb
Sergio Oliveira Jr <sergio.oliveira.jr@gmail.com>
2023-03-22 20:58:08 -0300

    Introducing set_activation_function so you can change it

--- lib/nn.rb
+++ lib/nn.rb
@@ -12,6 +12,8 @@ class Neuron
         @activation_function = activation_function
     end
 
+    attr_reader :weights, :bias
+
     def reset_params
         @initial_weights.each_with_index do |w,i|
             @weights[i].value = w
@@ -26,7 +28,9 @@ class Neuron
         (1...params.size).each { |i| @weights[i - 1].value = params[i] }
     end
 
-    attr_reader :weights, :bias
+    def set_activation_function(activation_function)
+        @activation_function = activation_function
+    end
 
     def parameters
         self.weights + [self.bias]
@@ -71,6 +75,11 @@ class Layer
         self.neurons.each { |n| n.reset_params }
     end
 
+    def set_activation_function(activation_function)
+        @activation_function = activation_function
+        self.neurons.each { |n| n.set_activation_function(activation_function) }
+    end
+
     def calc(inputs)
         outs = []
         self.neurons.each do |neuron|
@@ -86,37 +95,43 @@ class MLP
 
         number_of_layers = layers_config.size - 1 # last param is the activation function
 
-        act = layers_config.last
+        act_array = validate_act_array(layers_config.last, number_of_layers)
+
+        @layers = Array.new(number_of_layers - 1) # input layer is not really a layer object
+        (number_of_layers - 1).times do |i|
+            @layers[i] = Layer.new(layers_config[i], layers_config[i + 1], act_array[i])
+        end
+
+        @layers_config = layers_config
+    end
+
+    private def validate_act_array(act, number_of_layers)
 
         if !act.is_a?(Symbol) and !act.is_a?(Array)
             raise "Activation function must be passed as the last parameter: #{act.class} expected Symbol or Array of Symbols" 
         end
 
-        single_activation_function = nil
-
-        if act.is_a?(Symbol)
-
-            single_activation_function = act
-
-        else # is Array
+        if act.is_a?(Array)
 
             if not act.all? { |item| item.is_a?(Symbol) }
                 raise "Array with activation functions must contain symbols: #{act}"
             end
 
             if act.size == 1
-                single_activation_function = act.first
-            elsif act.size != number_of_layers - 1
+                return Array.new(number_of_layers - 1) { act.first } 
+            end
+
+            if act.size != number_of_layers - 1
                 raise "Array size does not match number of layers with activation functions: #{act.size} expected #{number_of_layers - 1}"
             end
-        end
-        
-        @layers = Array.new(number_of_layers - 1) # input layer is not really a layer object
-        (number_of_layers - 1).times do |i|
-            @layers[i] = Layer.new(layers_config[i], layers_config[i + 1], single_activation_function.nil? ? act[i] : single_activation_function)
-        end
 
-        @layers_config = layers_config
+            return act
+
+        else # is a Symbol
+
+            return Array.new(number_of_layers - 1) { act }
+
+        end
     end
 
     attr_reader :layers
@@ -177,6 +192,11 @@ class MLP
                 self.layers[li].neurons[ni].set_params(neuron)
             end
         end
+    end
+
+    def set_activation_function(activation_function)
+        act_array = validate_act_array(activation_function, @layers_config.size - 1)
+        self.layers.each_with_index { |layer, i| layer.set_activation_function(act_array[i]) }
     end
 
     def zero_grad
